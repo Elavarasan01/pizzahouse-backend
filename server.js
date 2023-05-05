c053a921339be6d7b5ca5f2eb1488aba194e2523
Elavarasan Senthilkumar <elavarasan.senthil@peopletech.com>
2023-05-05 17:55:16 +0530

    updated

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const http=require('http')
 const app=require('./app');
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://kselaa2010:Elavarasan@cluster0.hdrroe7.mongodb.net/?retryWrites=true&w=majority').then(console.log("db connected"))
 
 const port = process.env.PORT || 2000;
 
